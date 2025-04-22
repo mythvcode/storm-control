fb59179ce85872cb375efa0b394f717597bf7243
Alexander <shishebarov.a@selectel.ru>
2025-04-22 12:43:50 +0300

    Update kernel program

--- ebpfxdp/kernel/xdp_kernel.c
+++ ebpfxdp/kernel/xdp_kernel.c
@@ -9,7 +9,7 @@
 struct {
     __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
     __type(key, __u32);
-    __type(value, struct packet_counter);
+    __type(value, packet_counter);
     __uint(max_entries, CONFIG_MAP_MAX_ELEMENT);
 } intf_stats SEC(".maps");
 
@@ -52,48 +52,55 @@ static __always_inline __be16 get_h_proto(struct ethhdr *eth, void *data_end) {
     return vlan->h_vlan_encapsulated_proto;
 }
 
-static __always_inline int get_xdp_action(traffic_desc *tr_desc, __u32 ifindex, p_type pt){
+static __always_inline void increment_pass_stat(packet_counter *count_s, p_type pt) {
+    traffic_desc *desc[] = { &count_s->broadcast, &count_s->ipv4_mcast, 
+                             &count_s->ipv6_mcast, &count_s->other_mcast };
+
+    if (pt >= Broadcast && pt <= GenericMCast) {
+        desc[pt]->passed++;
+    }
+}
+
+static __always_inline int get_xdp_action(__u32 ifindex, p_type pt){
     drop_pkt *drop_desc = bpf_map_lookup_elem(&drop_intf, &ifindex);
+    packet_counter *count_s = bpf_map_lookup_elem(&intf_stats, &ifindex);
+    if (!count_s){
+        return XDP_PASS;
+    }
     if (!drop_desc){
-        tr_desc->passed++;
+        increment_pass_stat(count_s, pt);
         return XDP_PASS;
     }
     if (drop_desc->broadcast && pt == Broadcast){
-        tr_desc->dropped++;
+        count_s->broadcast.dropped++;
         return XDP_DROP;
     } else if (drop_desc->ipv4_mcast && pt == IPv4MCast){
-        tr_desc->dropped++;
+        count_s->ipv4_mcast.dropped++;
         return XDP_DROP;
     } else if (drop_desc->ipv6_mcast && pt == IPv6MCast){
-        tr_desc->dropped++;
+        count_s->ipv6_mcast.dropped++;
         return XDP_DROP;
     } else if (drop_desc->other_mcast && pt == GenericMCast){
-        tr_desc->dropped++;
+        count_s->other_mcast.dropped++;
         return XDP_DROP;
     }
-    tr_desc->passed++;
+    increment_pass_stat(count_s, pt);
     return XDP_PASS;
 }
 
 // calculate packets and return xdp_action
 static __always_inline int calculate_pkt(struct ethhdr *eth, __u32 ifindex, __u16 h_proto) {
-    struct packet_counter *count_s;
-    count_s = bpf_map_lookup_elem(&intf_stats, &ifindex);
-
-    if (!count_s){
-        return XDP_PASS;
-    }
     if (is_broadcast(eth->h_dest)){
-        return get_xdp_action(&(count_s->broadcast), ifindex, Broadcast);
+        return get_xdp_action(ifindex, Broadcast);
 
     } else if (is_ipv4_mcast(eth->h_dest) && h_proto==ETH_P_IP){
-        return get_xdp_action(&(count_s->ipv4_mcast), ifindex, IPv4MCast);
+        return get_xdp_action(ifindex, IPv4MCast);
 
     } else if (is_ipv6_mcast(eth->h_dest) && h_proto==ETH_P_IPV6){
-        return get_xdp_action(&(count_s->ipv6_mcast), ifindex, IPv6MCast);
+        return get_xdp_action(ifindex, IPv6MCast);
 
     } else if (is_other_mcast(eth->h_dest)){
-        return get_xdp_action(&(count_s->other_mcast), ifindex, IPv4MCast);
+        return get_xdp_action(ifindex, GenericMCast);
     }
 
     return XDP_PASS;
@@ -109,6 +116,6 @@ int storm_control(struct xdp_md *ctx)
     if (data + sizeof(struct ethhdr) > data_end){
         return XDP_PASS;
     }
-    return calculate_pkt(eth, ctx->ingress_ifindex, bpf_ntohs(get_h_proto(eth, data_end)));
 
+    return calculate_pkt(eth, ctx->ingress_ifindex, bpf_ntohs(get_h_proto(eth, data_end)));
 }
